658dc7f756df716fd1a8b7d4b16d645083f42367
Karsa <karsa@karsa.org>
2023-05-24 12:34:16 +0200

    Testing vercel deployment

--- scripts/writeReleaseMetadata.mjs
+++ scripts/writeReleaseMetadata.mjs
@@ -5,13 +5,10 @@ import fs from 'fs';
 import path from 'path';
 import { readSvgDirectory } from './helpers.mjs';
 
-const gitTmpPath = '/vercel/path0/';
-/*
 if (fs.existsSync(gitTmpPath)) {
   fs.rmSync(gitTmpPath, { recursive: true, force: true });
 }
 await simpleGit().clone(`https://${process.env.GITHUB_API_KEY}@github.com/lucide-icons/lucide.git`, gitTmpPath);
-*/
 const git = simpleGit(gitTmpPath);
 
 const currentDir = process.cwd();
